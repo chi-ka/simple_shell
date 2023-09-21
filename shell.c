8f249935c8140a19cd71ad49004ebc0d07740ce2
Chika Okafor <ochika6363@gmail.com>
2023-09-20 19:38:22 -0700

    shell

--- shell.c
+++ shell.c
@@ -10,100 +10,150 @@
 void executeCommand(char *cmd);
 void handleError(char *cmd);
 
-/**
- * main - Entry point for a simple shell.
- *
- * Return: Always 0.
- */
-
 int main() 
 {
-    char *cmd = NULL;
-    int is_interactive = isatty(STDIN_FILENO);
-    size_t cmd_size = 0;
-    ssize_t getline_result;
+	char *cmd = NULL;
+	int is_interactive = isatty(STDIN_FILENO);
+	size_t cmd_size = 0;
+	ssize_t getline_result;
 
-    while (1)
-    {
-        if (is_interactive)
-        {
-            write(STDOUT_FILENO, "#cisfun$ ", 9);
-        }
+	while (1)
+	{
+		if (is_interactive)
+		{
+			write(STDOUT_FILENO, ":) ", 3);
+		}
 
-        getline_result = getline(&cmd, &cmd_size, stdin);
+		getline_result = getline(&cmd, &cmd_size, stdin);
 
-        if (getline_result == -1)
-        {
-            if (is_interactive) 
-            {
-                write(STDOUT_FILENO, "\n", 1);
-            }
-            free(cmd);
-            exit(0);
-        }
+		if (getline_result == -1)
+		{
+			if (is_interactive) 
+			{
+				write(STDOUT_FILENO, "\n", 1);
+			}
+			free(cmd);
+			exit(0);
+		}
 
-        cmd[getline_result - 1] = '\0';
+		cmd[getline_result - 1] = '\0';
 
-        executeCommand(cmd);
-    }
+		executeCommand(cmd);
+	}
 }
-
-/**
- * executeCommand - Execute a command specified by 'cmd'.
- * @cmd: The command to execute.
- *
- */
 
 void executeCommand(char *cmd)
 {
-    char *token = strtok(cmd, " ");
-    char *argv[MAX_CMD_LEN];
-    int argc = 0;
-    pid_t pid;
-    int status;
+	char *token = strtok(cmd, " ");
+	char *argv[MAX_CMD_LEN];
+	int argc = 0;
+	pid_t pid;
+	int status;
 
-    while (token != NULL)
-    {
-        argv[argc++] = token;
-        token = strtok(NULL, " ");
-    }
+	while (token != NULL)
+	{
+		argv[argc++] = token;
+		token = strtok(NULL, " ");
+	}
 
-    argv[argc] = NULL;
+	argv[argc] = NULL;
 
-    if ((pid = fork()) == 0)
-    {
-        if (execve(argv[0], argv, NULL) == -1) 
-        {
-            handleError(cmd);
-            exit(1);
-        }
-    }
-    else if (pid > 0)
-    {
-        if (waitpid(pid, &status, 0) == -1) 
-        {
-            handleError(cmd);
-        }
-    }
-    else
-    {
-        handleError(cmd);
-    }
+	if (access(argv[0], X_OK) == 0)
+	{
+		if ((pid = fork()) == 0)
+		{
+			if (execve(argv[0], argv, NULL) == -1) 
+			{
+				handleError(cmd);
+				exit(1);
+			}
+		}
+		else if (pid > 0)
+		{
+			if (waitpid(pid, &status, 0) == -1) 
+			{
+				handleError(cmd);
+			}
+			else
+			{
+				if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+				{
+					handleError(cmd);
+				}
+			}
+		}
+		else
+		{
+			handleError(cmd);
+		}
+	}
+	else if (access(argv[0], F_OK) == -1)
+	{
+		char *path_copy, *path, *token;
+
+		path = getenv("PATH");
+		if (path == NULL)
+		{
+			perror("Failed to get PATH environment variable");
+			exit(EXIT_FAILURE);
+		}
+
+		path_copy = strdup(path); 
+		token = strtok(path_copy, ":");
+		while (token != NULL)
+		{
+			char executable[MAX_CMD_LEN];
+			snprintf(executable, sizeof(executable), "%s/%s", token, argv[0]);
+			if (access(executable, X_OK) == 0)
+			{
+				if ((pid = fork()) == 0)
+				{
+					if (execve(executable, argv, NULL) == -1)
+					{
+						handleError(cmd);
+						exit(1);
+					}
+				}
+				else if (pid > 0)
+				{
+					if (waitpid(pid, &status, 0) == -1)
+					{
+						handleError(cmd);
+					}
+					else
+					{
+						if (WIFEXITED(status) && WEXITSTATUS(status) != 0)
+						{
+							handleError(cmd);
+						}
+					}
+				}
+				else
+				{
+					handleError(cmd);
+				}
+				free(path_copy);
+				return;
+			}
+			token = strtok(NULL, ":");
+		}
+
+		free(path_copy);
+		printf("Command not found: %s\n", argv[0]);
+	}
+	else
+	{
+		handleError(cmd);
+	}
 }
-
-/**
- * handleError - Handle errors by printing an error message to STDERR.
- * @cmd: The command associated with the error.
- *
- */
 
 void handleError(char *cmd)
 {
-    char error_msg[128];
-    strcpy(error_msg, cmd);
-    strcat(error_msg, ": ");
-    strcat(error_msg, strerror(errno));
-    strcat(error_msg, "\n");
-    write(STDERR_FILENO, error_msg, strlen(error_msg));
+	char error_msg[128];
+	strcpy(error_msg, cmd);
+	strcat(error_msg, ": ");
+	strcat(error_msg, strerror(errno));
+	strcat(error_msg, "\n");
+	write(STDERR_FILENO, error_msg, strlen(error_msg));
 }
 
